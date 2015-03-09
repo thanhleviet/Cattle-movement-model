f6a87ac503609a895883dbce6af91fff9154f4f4
KarinaZile <karina.zile@gmail.com>
2015-03-09 18:09:55 +0000

    Updated code

--- movement.R
+++ movement.R
@@ -1,22 +1,27 @@
+############################
+# Cows move in herds, get infected
+############################
+
 library(lattice) # for plotting
 library(latticeExtra) # for plotting
 
 n <- 40 # total number of cows
+m <- 2 # number of herds
 a <- 500 # size of the feeding region in x-direction
 b <- 500 # size of the feeding region in y-direction
 t_max <- 100 # number of time steps
 speed <- 20 # speed of cow movement (from papers it is 5 km/h)
 prox <- 20 # max distance at which cow can be infected
-prob <- 0.9 # probability of infection when distance is =<prox
-distance <- 180 # average distance to the alpha cow
-a_prob <- 0.8 # probability of following alpha cow
+prob <- 0.9 # probability of infection when distance is <prox
+distance <- 10 # comfortable distance from your herd
+a_prob <- 0.8 # probability of following your herd
 
 x <- numeric(0) # vector of x-coordinates
 y <- numeric(0) # vector of y-coordinates
 SIR <- vector() # vector of infection status
 herd <- numeric(0) # identifies herd cow belongs to
-alpha_x <- rep(0,n) # position of relevant alpha cow
-alpha_y <- rep(0,n) # position of relevant alpha cow
+x_median <- rep(0,n) # median x-coordinates of your herd
+y_median <- rep(0,n) # median y-coordinates of your herd
 
 # Initial position of each cow is random, all susceptible
 # Herd 1 is in top-left corner, herd 2 is in bottom-right
@@ -36,92 +41,65 @@ for(i in 21:n)
   herd[i] <- 2 # Herd 2
 }
 
-H <- data.frame(x,y,SIR,herd,alpha_x,alpha_y) # organise initial info in a data frame
+H <- data.frame(x,y,SIR,herd,x_median,y_median) # organise initial info in a data frame
 H$SIR <- factor(SIR, levels=c("S","I","R")) #set SIR to be a factor
 
-H[2:3,3] <- "I" # introducing 2 infected animals in herd 1
+H[1:2,3] <- "I" # introducing 2 infected animals in herd 1
 
 par(ask=TRUE) # press Enter for every next plot,
 # this can be amended to produce GIF or other animation/video 
 
-### Let animal 1 and animal 21 be alpha cows ###
 
-for (k in 1: t_max) # main loop that updates H at each time step
-{ # update first alpha cow
-  angle <- runif(1, min = 0, max = 2*pi) # choose random angle
-  x_new <- H[1,1] + cos(angle)*speed
-  y_new <- H[1,2] + sin(angle)*speed
-  
-  while (x_new<0 | x_new>a | y_new<0 | y_new>b)
-  { # if travelling in this direction cow will collide with the 
-    # fence, cow will not choose this direction, choose another
-    # random angle and keep checking that cow stays in feeding region
-    angle <- runif(1, min = 0, max = 2*pi)
-    x_new <- H[1,1] + cos(angle)*speed
-    y_new <- H[1,2] + sin(angle)*speed
-  }
-  H[1,1] <- x_new # update x 
-  H[1,2] <- y_new # and y coordinates
-  H[which(H[,4]==1),5] <- H[1,1] # and alpha cow position for 
-  H[which(H[,4]==1),6] <- H[1,2] # cows in relevant herd
-  
-  # update second alpha cow
-  angle <- runif(1, min = 0, max = 2*pi) # choose random angle
-  x_new <- H[21,1] + cos(angle)*speed
-  y_new <- H[21,2] + sin(angle)*speed
-  
-  while (x_new<0 | x_new>a | y_new<0 | y_new>b)
-  { # if travelling in this direction cow will collide with the 
-    # fence, cow will not choose this direction, choose another
-    # random angle and keep checking that cow stays in feeding region
-    angle <- runif(1, min = 0, max = 2*pi)
-    x_new <- H[21,1] + cos(angle)*speed
-    y_new <- H[21,2] + sin(angle)*speed
-  }
-  H[21,1] <- x_new # update x 
-  H[21,2] <- y_new # and y coordinates
-  H[which(H[,4]==2),5] <- H[21,1] # and alpha cow position for 
-  H[which(H[,4]==2),6] <- H[21,2] # cows in relevant herd
-    
- for(i in c(2:20,22:n)) # loop that updates non-alpha cow position
- {
-   angle <- runif(1, min = 0, max = 2*pi) # choose random angle
-   x_new <- H[i,1] + cos(angle)*speed # update x 
-   y_new <- H[i,2] + sin(angle)*speed # and y coordinates
-   if (sqrt((x_new-H[i,5])^2 + (y_new-H[i,6])^2)>distance)
-   { # if distance to the alpha cow is > than average
-     q <- runif(1, min = 0, max = 1)
-     if (q<a_prob) # then with a_probability cow might stay in proximity
-     {
-       while (sqrt((x_new-H[i,5])^2 + (y_new-H[i,6])^2)>distance)
-       { 
-         angle <- runif(1, min = 0, max = 2*pi)
-         x_new <- H[i,1] + cos(angle)*speed 
-         y_new <- H[i,2] + sin(angle)*speed 
-       }
-     }
-   }
-   H[i,1] <- x_new # update x 
-   H[i,2] <- y_new # and y coordinates
-      
-  if (H[i,3] == "I") # for each infected cow
+for (i in 1: t_max) # main loop that updates H at each time step
+{
+  for (j in 1:m) # for each herd find the median coordinates
   {
-    for (j in 1:n) # check each susceptible cow 
+   H[which(H[,4]==j),5] <- median(H[which(H[,4]==j),1])
+   H[which(H[,4]==j),6] <- median(H[which(H[,4]==j),2])
+  }
+  for(k in 1:n) # update the location of each cow
+  {
+   q <- runif(1, min = 0, max = 1)
+   if (sqrt((H[k,1]-H[k,5])^2+(H[k,2]-H[k,6])^2)>distance & q<a_prob)
+   { # if a cow is too far, with prob. q it will come back
+     ax <- H[k,5] - H[k,1]
+     ay <- H[k,6] - H[k,2]
+     epsilon <- pi/12 * runif(1, min=-1, max=1)
+     angle <- atan2(ay,ax) + epsilon
+     H[k,1] <- H[k,1] + cos(angle)*speed
+     H[k,2] <- H[k,2] + sin(angle)*speed
+   } else
+   {
+     angle <- runif(1, min = -pi, max = pi) # choose random angle
+     x_new <- H[k,1] + cos(angle)*speed
+     y_new <- H[k,2] + sin(angle)*speed
+     while (x_new<0 | x_new>a | y_new<0 | y_new>b)
+     { # if travelling in this direction cow will collide with the 
+       # fence, cow will not choose this direction, choose another
+       # random angle and keep checking that cow stays in feeding region
+       angle <- runif(1, min = -pi, max = pi)
+       x_new <- H[k,1] + cos(angle)*speed
+       y_new <- H[k,2] + sin(angle)*speed
+     }
+     H[k,1] <- x_new # update x 
+     H[k,2] <- y_new # and y coordinates
+   }
+  }
+  for(l in 1:n) {
+    if (H[l,3] == "I") # for each infected cow !!!COUNT AND RECOVER
     {
-      if (H[j,3] == "S" & sqrt((H[i,1]-H[j,1])^2 + (H[i,2]-H[j,2])^2)<prox)
-      { # and if distance between them is <prox
+      for (m in 1:n) { # check each susceptible cow 
         p <- runif(1, min = 0, max = 1)
-        if (p<prob) # then cow gets infected with certain probability
-        {
-          H[j,3] = "I"
-        }
-      }
-    }
-  } 
- } # plot location of the cows (susceptible - empty, infected - colored)
-susceptible <- xyplot(y~x, groups=herd, data=H[(H$SIR=="S"),], pch=1, xlim=c(0,a), ylim=c(0,b))
-infected <- xyplot(y~x, groups=herd, data=H[(H$SIR=="I"),], pch=15, xlim=c(0,a), ylim=c(0,b))
-print(susceptible+infected)
+        if (H[m,3]=="S" & sqrt((H[m,1]-H[l,1])^2+(H[m,2]-H[l,2])^2)<prox & p<prob) {
+          H[m,3] <- "I" # and if distance between them is <prox
+        } # then cow gets infected with probability p
+      } 
+    } 
+  } # plot location of the cows (susceptible - empty, infected - colored)
+  susceptible <- xyplot(y~x, groups=herd, data=H[(H$SIR=="S"),], pch=1, xlim=c(0,a), ylim=c(0,b))
+  infected <- xyplot(y~x, groups=herd, data=H[(H$SIR=="I"),], pch=15, xlim=c(0,a), ylim=c(0,b))
+  print(susceptible+infected)
 }
 
-
+  
+    
